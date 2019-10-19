f45296c68f884607764f94c0c0485123c85dc3a6
chinatiger99 <davidchen@Davids-MacBook-Pro-2.local>
2019-10-19 05:17:23 -0500

    Added power select

--- project/src/App.js
+++ project/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import XLSX from 'xlsx';
@@ -12,29 +12,72 @@ import Dropdown from "react-bootstrap/Dropdown";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import { VictoryBar,VictoryChart,VictoryAxis,VictoryTheme } from 'victory';
+import { PowerSelect } from 'react-power-select'
+ import 'react-power-select/dist/react-power-select.css'
+
+import "./App.css";
+
+var json = require('./data.json'); //(with path)
+var strategy = '';
+
+var strategies;
+var strategy;
 
 
-import "./App.css"
+function handleChange(strat) {
+    console.log(strat.option);
+    strategy = strat.option; 
+}
 
 const StrategiesDropdown = ({ children }) => {
-    const [show, toggleShow] = React.useState(true);
+    //const [show, toggleShow] = React.useState(true);
+    const [strategy] = React.useState("");
+    
+    setStrategy()
+
+    var targetMet = "Currently on pace to meet target";
+    var vals = getStrategyCount('hello!');
+    var totalCount = vals[0];
+    var totalTotal = vals[1];
+    var projected = vals[2];
+    if(projected < totalTotal) {
+        targetMet = "Currently NOT on pace to meet target";
+    }
+    console.log(targetMet);
     console.log(chart_values);
+    console.log('strategies');
+    
+    strategies = getStrategies();
+
+    function setStrategy(state, action) {
+        this.strategy = state;
+    }
+
     return (
         <>
-            <Dropdown>
+            {/* <PowerSelect
+                options={strategies}
+            /> */}
+            <PowerSelect
+                options={strategies}
+                selected={this.strategy}
+                onChange={setStrategy}
+            />
+            {/* <Dropdown>
                 <Dropdown.Toggle variant="success" id="dropdown-basic">
                     Strategies
                     <span role="img" aria-label="tada">
                         🎉
                     </span>
                 </Dropdown.Toggle>
-
+                
                 <Dropdown.Menu>
-                    <Dropdown.Item href="#/action-1">Strategy One</Dropdown.Item>
+                    
+                    {/* <Dropdown.Item href="#/action-1">Strategy One</Dropdown.Item>
                     <Dropdown.Item href="#/action-2">Strategy Two</Dropdown.Item>
-                    <Dropdown.Item href="#/action-3">Strategy Three</Dropdown.Item>
-                </Dropdown.Menu>
-            </Dropdown>
+                    <Dropdown.Item href="#/action-3">Strategy Three</Dropdown.Item> */}
+                {/* </Dropdown.Menu> */}
+            {/* </Dropdown> */} 
         </>
     );
 };
@@ -44,6 +87,8 @@ const App = () => (
         <Jumbotron fluid>
             <h1 className="header">United Way of San Antonio</h1>
         </Jumbotron>
+
+        
         <StrategiesDropdown className="dropdown-strategies">
             Strategies
         </StrategiesDropdown>
@@ -76,6 +121,37 @@ const data = [
     {quarter: 4, earnings: 19000}
   ];
   
-  
+function getStrategyCount(strategy) {
+    var totalCount = 0;
+    var totalTarget = 0;
+    console.log(strategy);
+    console.log('json length is ' + json.length);
+    var set = new Set();
+    for(var i = 0; i < json.length; i++) {
+        set.add(json[i]['Strategy_Name']);
+        // if(json[i]['Strategy_Name'] == "Provide and/or build capacity for organizations that offer education and workforce development to clients at or below 200% of FPL. (Indicator 1)") {
+        //     //totalCount += json[i]["Unique Count July 2019"];
+        //     //totalCount += json[i]["Unique Count August 2019"];
+        //     //totalCount += json[i]["Unique Count September 2019"];
+        //     if(json[i]["Annual Target (if applicable)"] != "") {
+        //         totalTarget += json[i]["Annual Target (if applicable)"];
+        //     } 
+        // }
+    }
+    console.log(set);
+    console.log(totalCount + ' and target is ' + totalTarget);
+    return[totalCount, totalTarget, 0];
+}
+
+function getStrategies() {
+    var set = new Set();
+    for(var i = 0; i < json.length; i++) {
+        set.add(json[i]['Strategy_Name']);
+    }
+    var strats = Array.from(set);
+    console.log('strats');
+    console.log(strats);
+    return strats;
+}
 
 export default App;
